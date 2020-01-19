8ccc8418e1ab6946fead4ae52fe2ad7096c22163
deb <debmalya.biswas25@gmail.com>
2020-01-19 19:45:36 +0530

    Initial login

---
+++ src/test/java/deb/practice/microservice/microservicepractice/MicroservicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package deb.practice.microservice.microservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
