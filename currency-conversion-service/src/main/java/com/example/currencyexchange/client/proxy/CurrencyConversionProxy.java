01caff3ac603c55c05c5665aa55bf8274582382a
deb <debmalya.biswas25@gmail.com>
2020-01-25 18:58:05 +0530

    Added ribbon client

--- currency-conversion-service/src/main/java/com/example/currencyexchange/client/proxy/CurrencyConversionProxy.java
+++ currency-conversion-service/src/main/java/com/example/currencyexchange/client/proxy/CurrencyConversionProxy.java
@@ -1,11 +1,13 @@
 package com.example.currencyexchange.client.proxy;
 
 import com.example.currencyexchange.model.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "forex-service", url = "localhost:8000")
+@FeignClient(name = "forex-service")
+@RibbonClient(name = "forex-service")
 public interface CurrencyConversionProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
